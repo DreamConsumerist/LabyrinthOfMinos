b9eb0abfa94710c410f5ff3537ab14c9113b465c
Brenden Boehme <118089982+DreamConsumerist@users.noreply.github.com>
2025-10-04 23:35:31 -0800

    A* work continues

--- Labyrinth of Minos Unity Project/Assets/Scripts/A_StarPathfinding.cs
+++ Labyrinth of Minos Unity Project/Assets/Scripts/A_StarPathfinding.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 
 public static class A_StarPathfinding
 {
+    public class Node
+    {
+        float g_cost, h_cost;
+        float FCost => g_cost + h_cost;
+
+        Node cameFrom;
+        public Node() { }
+    }
     public static List<Vector2Int> FindPath(Vector2Int currentPos, Vector2Int targetPos, bool[,] maze)
     {
         
