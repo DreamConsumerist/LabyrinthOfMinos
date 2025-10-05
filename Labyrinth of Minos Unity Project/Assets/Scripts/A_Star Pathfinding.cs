0e7d5b282e56dba9ad155ab2d57d797e9771147c
Brenden Boehme <118089982+DreamConsumerist@users.noreply.github.com>
2025-10-04 23:07:14 -0800

    Beginning A* Work

---
+++ Labyrinth of Minos Unity Project/Assets/Scripts/A_Star Pathfinding.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public static class A_StarPathfinding
+{
+    public static List<Vector2Int> FindPath(Vector2Int currentPos, Vector2Int targetPos, bool[,] maze)
+    {
+        
+        
+        List<Vector2Int> path = new List<Vector2Int>();
+        return path;
+    }
+}
