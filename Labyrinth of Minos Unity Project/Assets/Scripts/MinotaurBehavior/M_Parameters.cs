050604764c2313e197173d5856fd69a089a22e9a
Brenden Boehme <118089982+DreamConsumerist@users.noreply.github.com>
2025-11-07 15:22:46 -0900

    Basic KillsPlayerState

--- Labyrinth of Minos Unity Project/Assets/Scripts/MinotaurBehavior/M_Parameters.cs
+++ Labyrinth of Minos Unity Project/Assets/Scripts/MinotaurBehavior/M_Parameters.cs
@@ -3,17 +3,19 @@ using UnityEngine;
 public class MinotaurParameters : MonoBehaviour
 {
     // Vision parameters
-    [SerializeField] public float visionDistance = 30f;
-    [SerializeField] public float visionCone = 60f;
+    public float visionDistance = 30f;
+    public float visionCone = 60f;
 
     // Movement parameters
-    [SerializeField] public float staticRotateAngle = 90f;
-    [SerializeField] public float patrolWalkSpeed = 1.0f;
-    [SerializeField] public float patrolRotateSpeed = 75f;
-    [SerializeField] public float chaseRunSpeed = 3.0f;
-    [SerializeField] public float chaseRotateSpeed = 90f;
+    public float staticRotateAngle = 90f;
+    public float patrolWalkSpeed = 1.0f;
+    public float patrolRotateSpeed = 75f;
+    public float chaseRunSpeed = 3.0f;
+    public float chaseRotateSpeed = 90f;
 
-    [SerializeField] public float maxChaseTime = 15f;
+    // Time based state checkers
+    public float maxChaseTime = 15f;
+    public float killTime = 5f;
 
-    [SerializeField] public float pointRadius = .6f;
+    public float pointRadius = .6f;
 }
