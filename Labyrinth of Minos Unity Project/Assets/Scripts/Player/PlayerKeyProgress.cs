fb3ee6f6c61b54862a1d9dada4e6268e2b1fa5fd
skully21ak <elmquistalex03@gmail.com>
2025-11-08 21:01:18 -0900

    added sequential ordering
    keys must be picked up in order, plus changed map size and such

---
+++ Labyrinth of Minos Unity Project/Assets/Scripts/Player/PlayerKeyProgress.cs
@@ -0,0 +1,20 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerKeyProgress : NetworkBehaviour
+{
+    // 1,2,3 next needed key index; starts at 1
+    public NetworkVariable<int> NextKeyIndex = new NetworkVariable<int>(1,
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Server);
+
+    // Optional: simple helper to reset per life/round
+    [ServerRpc(RequireOwnership = false)]
+    public void ResetProgressServerRpc()
+    {
+        NextKeyIndex.Value = 1;
+    }
+
+    // Convenience check
+    public bool HasAllKeys => NextKeyIndex.Value > 3;
+}
