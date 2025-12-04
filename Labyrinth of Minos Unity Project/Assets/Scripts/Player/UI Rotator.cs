748ff6fe7fb82bac331a3a297acb038df0c647a4
Martncox <Lutherak10@gmail.com>
2025-12-04 12:57:12 -0900

    ///8

--- Labyrinth of Minos Unity Project/Assets/Scripts/Player/UI Rotator.cs
+++ Labyrinth of Minos Unity Project/Assets/Scripts/Player/UI Rotator.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 
 public class UIRotator : MonoBehaviour
 {
@@ -16,3 +16,4 @@ public class UIRotator : MonoBehaviour
         transform.Rotate(offset);
     }
 }
+*/
